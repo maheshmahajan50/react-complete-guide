abc0dd65509850f8a549c42d8f7a3a66edb0fa7e
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-10-17 15:13:53 +0530

    Hmmm... enough JSX, Lets use props now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <ExpenseItem/>;
+  const expenses = [
+    {
+      id: "e1",
+      title: "First title",
+      price: 344.44,
+      date: new Date(2022, 3, 28),
+    },
+    {
+      id: "e2",
+      title: "Second title",
+      price: 453.44,
+      date: new Date(2022, 4, 12),
+    },
+    {
+      id: "e3",
+      title: "Third title",
+      price: 123.44,
+      date: new Date(2022, 3, 4),
+    },
+  ];
+  return (
+    <>
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+    </>
+  );
 }
 
 export default App;
