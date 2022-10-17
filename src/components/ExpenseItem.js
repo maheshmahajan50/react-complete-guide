9a02f0802793e31dd65f692856eaee5d55101f1d
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-10-17 14:45:46 +0530

    lets add some css to look nicer

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
-  return <h2>Expense Items...</h2>
+  return(
+    <div className='expense-item'>
+      <div>March 21, 2020</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>34242 RS.</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
