92d2f8ed6bb4efef04752929c754ee92988d89da
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-10-18 11:44:01 +0530

    This is how we use event handlers in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,16 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickMe = () => {
+    console.log("clicked!!!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickMe}>Click here....</button>
       </div>
     </Card>
   );
