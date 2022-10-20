d3a73aaf02b152b5298b913bf4687e9261f88373
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-10-20 13:36:27 +0530

    Wooho git add .! Closing form button introduced

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setEdting] = useState(false);
+
   const onSaveExpenseDataHandler = (data) => {
     const expense = {
       ...data,
       id: Math.random().toString(),
     };
     props.onAddExpense(expense);
+    setEdting(false);
   };
+
+  const startEditingHandler = () => {
+    setEdting(true);
+  };
+
+  const stopEdtingHandler = () => {
+    setEdting(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add a New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={stopEdtingHandler}
+        />
+      )}
     </div>
   );
 };
